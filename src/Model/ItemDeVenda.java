abe55f1921c10ad561c1203fa0b1728bb34c061e
João Langa <langajoao16@gmail.com>
2024-11-03 20:30:26 +0200

    Primeira classe

---
+++ src/Model/ItemDeVenda.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author mac
+ */
+public interface ItemDeVenda<T> {
+    String getNome();
+    double getPreco();
+    void setPreco(double preco);
+}
---
+++ src/Model/Pizza.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author mac
+ */
+public class Pizza implements ItemDeVenda<Pizza> {
+    private String nome;
+    private double preco;
+
+    // Construtor
+    public Pizza(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    @Override
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    // Outros métodos específicos de Pizza
+}
+
